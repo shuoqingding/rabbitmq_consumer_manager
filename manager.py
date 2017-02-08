48f0620aabc2a07014a949f03b79a7a62f7f17ee
Shuoqing Ding <dsq704136@gmail.com>
2017-02-08 15:49:53 +0800

    Added connection reconnect

--- manager.py
+++ manager.py
@@ -6,6 +6,10 @@ import time
 from consumer import Consumer
 from urllib import quote
 
+
+class OpenConnectionTimeoutException(Exception):
+    pass
+
 class RabbitmqAdmin(object):
     admin_url = 'http://127.0.0.1:15672/api'
     uname = 'guest'
@@ -40,29 +44,54 @@ class ConsumerManager(object):
         self.conn_num = conn_num
         self.conns = []
         self.conn_index = 0
+        self.rabbitmq_admin = RabbitmqAdmin()
+
         # Create the connection pool
         for i in range(conn_num):
-            conn = pika.SelectConnection(pika.URLParameters(self.amqp_url))
-            t = threading.Thread(target=conn.ioloop.start)
-            t.setDaemon(True)
-            t.start()
+            conn = self.create_connection()
             self.conns.append(conn)
 
-        self.rabbitmq_admin = RabbitmqAdmin()
+    def create_connection(self, ):
+        conn = pika.SelectConnection(pika.URLParameters(self.amqp_url))
+        t = threading.Thread(target=conn.ioloop.start)
+        t.setDaemon(True)
+        t.start()
+
+        timeout = 10  # seconds
+        warn_timeout = 2
+
+        # Wait until the connection is open
+        start = time.time()
+        while not conn.is_open:
+            t = time.time() - start
+            if t > warn_timeout:
+                print "It took too long to open the connection"
+            elif t > timeout:
+                raise OpenConnectionTimeoutException()
+            time.sleep(0.1)
+
+        return conn
 
     def get_queues(self, ):
         return self.rabbitmq_admin.list_queues(cols=['name', 'messages',
                                                      'messages_ready'])
 
     def create_consumer_for_queue(self, queue):
-        # TODO: restart connection if closed
-        assert self.conns[self.conn_index].is_open
-        c = Consumer(self.conns[self.conn_index], self.amqp_url, queue)
-        c.setDaemon(True)
-        c.start()
-
         # Choose the connection in the round-robin way
         self.conn_index = (self.conn_index + 1) % self.conn_num
+        conn = self.conns[self.conn_index]
+
+        # Restart the connection if it is closed
+        if not conn.is_open:
+            conn = self.create_connection()
+            self.conns[self.conn_index] = conn
+
+        assert conn.is_open, "Failed to create a connection"
+
+        # Start a consumer thread
+        c = Consumer(conn, self.amqp_url, queue)
+        c.setDaemon(True)
+        c.start()
 
         self.consumer_by_queue[queue] = c
 
@@ -75,6 +104,7 @@ class ConsumerManager(object):
 
                 # Clean consumers of empty queues
                 if q['messages'] == 0 and consumer is not None:
+                    # TODO: handle pika.exceptions.ConnectionClosed
                     self.consumer_by_queue[qname].stop()
                     del self.consumer_by_queue[qname]
 
